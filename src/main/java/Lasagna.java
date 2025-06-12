742edf89568afb32a95bd916361df94774b83dc9
Amirhossein Emadi <emadi.henker404@gmail.com>
2025-06-12 11:01:05 -0700

    refactor: update constants to be more readable

--- src/main/java/Lasagna.java
+++ src/main/java/Lasagna.java
@@ -1,20 +1,20 @@
 public class Lasagna {
-    // 1. Define the 'expectedMinutesInOven()' method
+
+    private static final int EXPECTED_MINUTES_IN_OVEN = 40;
+    private static final int PREPARATION_TIME_PER_LAYER = 2;
+
     public int expectedMinutesInOven() {
-        return 40;
+        return EXPECTED_MINUTES_IN_OVEN;
     }
 
-    // 2. Define the 'remainingMinutesInOven()' method
     public int remainingMinutesInOven(int actualMinutes) {
         return expectedMinutesInOven() - actualMinutes;
     }
 
-    // 3. Define the 'preparationTimeInMinutes()' method
     public int preparationTimeInMinutes(int layers) {
-        return layers * 2; // 2 min per layer
+        return layers * PREPARATION_TIME_PER_LAYER;
     }
 
-    // 4. Define the 'totalTimeInMinutes()' method
     public int totalTimeInMinutes(int layers, int actualMinutes) {
         return preparationTimeInMinutes(layers) + actualMinutes;
     }
